8239d156814468143113a765f51fff94d4b94d81
Théo Faure <faure.theo@outlook.fr>
2017-08-16 14:16:48 +0100

    change mutant to adversary

--- framework/forms.py
+++ framework/forms.py
@@ -11,7 +11,7 @@ class CreateMutantsForm(forms.Form):
     strategy = forms.ModelChoiceField(queryset=Strategy.objects.all())
     validation = forms.ModelChoiceField(queryset=Validation.objects.all())
     chatbot = forms.ModelChoiceField(queryset=Chatbot.objects.all())
-    nb_per_mutant = forms.IntegerField(max_value=100, min_value=1)
+    nb_max_adversary_per_utterance = forms.IntegerField(max_value=100, min_value=1)
 
 class GetAnsMutantsForm(forms.Form):
     strategy = forms.ModelChoiceField(queryset=Strategy.objects.all())
